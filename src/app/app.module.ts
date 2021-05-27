99021122ac8c629c6924ab3830267ee0b2a3d554
stay7 <codeLatte20@gmail.com>
2021-05-27 15:32:39 +0900

    Create conroller, service files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Group } from 'src/group/group.entity';
 import { User } from 'src/user/user.entity';
-import { AppController } from '../controllers/app.controller';
-import { AppService } from '../services/app.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot()],
