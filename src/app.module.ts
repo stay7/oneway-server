6d03dd638140522d85542377163665d003941ec6
stay7 <codeLatte20@gmail.com>
2021-06-09 17:09:03 +0900

    Refactor directory structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModules } from '../auth/auth.modules';
-import { WordsModule } from '../words/words.module';
-import * as ormConfig from '../../ormconfig';
-import { GroupsModule } from '../groups/groups.module';
+import { AuthModules } from './auth/auth.modules';
+import { WordsModule } from './words/words.module';
+import * as ormConfig from '../ormconfig';
+import { GroupsModule } from './groups/groups.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormConfig),
