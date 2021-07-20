4bfd0a073a3d3b96ed1afcd66958d1a469e976d1
stay7 <codeLatte20@gmail.com>
2021-07-21 03:16:00 +0900

    Fix import jsonwebtoken

--- src/types/token.ts
+++ src/types/token.ts
@@ -1,9 +1,10 @@
-import jwt from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 
 export class Token {
   constructor(public token: string) {}
 
   get expireDate() {
+    console.log(this.token);
     const decodedToken = jwt.decode(this.token);
     return new Date(decodedToken['exp']);
   }
